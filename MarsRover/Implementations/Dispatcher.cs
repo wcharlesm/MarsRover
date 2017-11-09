adcbeaa54268e55240a9ca01d486bea51c955c0b
Bill <wcharlesm@gmail.com>
2017-11-08 22:30:40 -0500

    create project with IDispatcher entry point

---
+++ MarsRover/Implementations/Dispatcher.cs
@@ -0,0 +1,15 @@
+﻿using MarsRover.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.Implementations
+{
+    class Dispatcher : IDispatcher
+    {
+        string IDispatcher.ReceiveInstructions(string dispatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarsRover/Interfaces/IDispatcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.Interfaces
+{
+    public interface IDispatcher
+    {
+        String ReceiveInstructions(String dispatch);
+    }
+}
